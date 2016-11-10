e7e431716a8f62674c86490df9618980cce23c04
WeiTingChen1217 <t104368022@ntut.org.tw>
2016-11-10 13:52:09 +0800

    建立作業五

---
+++ HW5/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HW5
+//
+//  Created by WeiTingChen on 11/10/16.
+//  Copyright © 2016 WeiTingChen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "This is homework5_2_a.\n\n";
+    
+    
+    
+    return 0;
+}
