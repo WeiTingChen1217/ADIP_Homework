c6bc0fd499a8d2664039d80d5761e6c034c234a1
WeiTingChen1217 <t104368022@ntut.org.tw>
2016-11-19 09:38:28 +0800

    建立分支HW6

---
+++ HW6/HW6_2_a.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW6
+//
+//  Created by WeiTingChen on 11/17/16.
+//  Copyright © 2016 WeiTingChen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
